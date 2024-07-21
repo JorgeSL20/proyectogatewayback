b4373f85c8b3159b3342994d066a102f8638875b
KitoS2001 <20200722@uthh.edu.mx>
2024-07-21 05:36:13 -0600

    ultimo

--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { CreateLoginDto } from './dto/create-login.dto';
+import { UpdateLoginDto } from './dto/update-login.dto';
 import { ValidarLogin } from 'src/auth/dto/ValidLoginDto-auth';
 import * as bcryptjs from 'bcryptjs';
 import { AuthService } from 'src/auth/auth.service';
@@ -11,62 +13,37 @@ import { Logs } from 'src/auth/entities/logs.entity';
 export class LoginService {
 
   constructor(
-    @InjectRepository(Auth) private authRepository: Repository<Auth>, 
+    @InjectRepository(Auth) private authRepository: Repository<Auth>,
     private authService: AuthService,
     @InjectRepository(Logs) private logsRepository: Repository<Logs>
   ) { }
 
   async validLogin(createLoginDto: ValidarLogin): Promise<boolean> {
     const data = await this.authService.getUser(createLoginDto.email);
-    if (!data) {
-      return false;
-    }
-
-    // Check if the number of attempts is greater than or equal to 5
-    if (data.intentos >= 5) {
-      // If the attempts have reached the limit, set a timer to reset the attempts after 5 minutes
-      this.resetearIntentos(data.id);
-      throw new Error('Número máximo de intentos alcanzado. Intenta de nuevo en 5 minutos.');
-    }
-
-    const isPasswordValid = await bcryptjs.compare(createLoginDto.password, data.password);
-    if (isPasswordValid) {
-      // Reset attempts on successful login
-      await this.resetearIntentos(data.id);
+    if (data && await bcryptjs.compare(createLoginDto.password, data.password)) {
       return true;
     } else {
-      // Increment attempts on unsuccessful login
-      await this.asignarIntentos(data.id, data.intentos + 1);
       return false;
     }
   }
 
-  asignarIntentos(id: number, intento: number) {
-    this.authRepository.query(
-      "UPDATE usuarios SET intentos = " + intento + " WHERE id = " + id + ""
-    );
+  async asignarIntentos(id: number, intento: number) {
+    await this.authRepository.update(id, { intentos: intento });
   }
 
-  resetearIntentos(id: number) {
-    console.log("conteo iniciado");
-    setTimeout(() => {
-      this.authRepository.query(
-        "UPDATE usuarios SET intentos = 0 WHERE id = " + id + ""
-      );
+  async resetearIntentos(id: number) {
+    // Resetea los intentos después de 5 minutos
+    setTimeout(async () => {
+      await this.authRepository.update(id, { intentos: 0 });
       console.log("Intentos reseteados");
-    }, 300000); // 5 minutos = 300000 ms
+    }, 300000); // 300000 ms = 5 minutos
   }
 
   async crearLogs(data: { accion: string, ip: string, url_solicitada: string, status: number, fecha: string }, email: string) {
-    const userFound = await this.authRepository.findOne({
-      where: {
-        email: email
-      }
-    });
-    const newLog = this.logsRepository.create({
-      usuario: userFound,
-      ...data
-    });
-    this.logsRepository.save(newLog);
+    const userFound = await this.authRepository.findOne({ where: { email: email } });
+    if (userFound) {
+      const newLog = this.logsRepository.create({ usuario: userFound, ...data });
+      await this.logsRepository.save(newLog);
+    }
   }
 }
