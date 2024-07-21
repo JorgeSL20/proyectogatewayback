1bb243e20efbbb04a35bd28515a1da1f588ca56c
KitoS2001 <20200722@uthh.edu.mx>
2024-07-21 02:25:19 -0600

    ultimo

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,81 +1,71 @@
-import { Controller, Post, Body, HttpException, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpException, HttpStatus } from '@nestjs/common';
 import { LoginService } from './login.service';
 import { CreateLoginDto } from './dto/create-login.dto';
+import { UpdateLoginDto } from './dto/update-login.dto';
+import { ValidarLogin } from 'src/auth/dto/ValidLoginDto-auth';
 import { AuthService } from 'src/auth/auth.service';
 
 @Controller('login')
 export class LoginController {
 
-  constructor(private readonly loginService: LoginService, private userService: AuthService) {}
+  constructor(private readonly loginService: LoginService, private userService:AuthService) {}
+
+  intento:number = 0;
 
   @Post()
   async validLogin(@Body() createLoginDto: CreateLoginDto) {
-    try {
-      const datos = await this.userService.getUser(createLoginDto.email);
-      if (!datos) throw new HttpException('Usuario no encontrado', HttpStatus.NOT_FOUND);
+    console.log(createLoginDto.password)
+    try 
+    {
+      console.log("entra")
+      const datos = await this.userService.getUser(createLoginDto.email)
+      if(datos === null) throw new Error("error")
 
-      const ahora = new Date();
-      const fechaUltimoIntento = datos.fechaUltimoIntento ? new Date(datos.fechaUltimoIntento) : null;
-      const tiempoTranscurrido = fechaUltimoIntento ? (ahora.getTime() - fechaUltimoIntento.getTime()) / 60000 : null; // En minutos
-
-      if (datos.intentos >= 5 && (tiempoTranscurrido < 5)) {
+      this.intento = datos.intentos;
+      if(this.intento === 5){  
         return {
-          message: 'Número máximo de intentos alcanzado. Intenta de nuevo en 5 minutos.',
-          status: HttpStatus.CONFLICT,
-          nIntentos: datos.intentos
-        };
-      } else {
-        let intento = datos.intentos;
-
-        if (tiempoTranscurrido >= 5) { // Si han pasado 5 minutos, reinicia los intentos
-          intento = 0;
-        }
-
-        if (intento >= 5) {
-          await this.loginService.resetearIntentos(datos.id);
-          await this.loginService.crearLogs({
-            accion: 'Sesión bloqueada',
-            fecha: createLoginDto.fecha,
-            ip: createLoginDto.ip,
-            status: 409,
-            url_solicitada: '/login'
-          }, datos.email);
-
-          return {
-            message: 'Número máximo de intentos alcanzado. Intenta de nuevo en 5 minutos.',
-            status: HttpStatus.CONFLICT,
-            nIntentos: intento
-          };
-        } else {
-          intento++;
-          await this.loginService.asignarIntentos(datos.id, intento, ahora);
-
-          const isValidLogin = await this.loginService.validLogin(createLoginDto);
-          if (isValidLogin) {
-            await this.loginService.resetearIntentos(datos.id);
-            await this.loginService.crearLogs({
-              accion: 'Inicio de sesión',
-              fecha: createLoginDto.fecha,
-              ip: createLoginDto.ip,
-              status: 200,
-              url_solicitada: '/login'
-            }, datos.email);
-
+        message: 'Numero de maxinmo de intentos alcanzado',
+        status: HttpStatus.CONFLICT,
+        nIntentos: this.intento
+      }}
+      else
+      {
+        this.intento++;
+        this.loginService.asignarIntentos(datos.id,this.intento)
+          if(this.intento >= 5)
+          {
+            console.log("la de abajo")
+            this.loginService.resetearIntentos(datos.id)
+            this.loginService.crearLogs({accion:'Sesion bloqueada',fecha:createLoginDto.fecha,ip:createLoginDto.ip,status:409,url_solicitada:'/login'},datos.email)
             return {
-              message: 'Login correcto',
-              status: 200,
-              token: datos.id
-            };
-          } else {
-            return {
-              message: 'Login incorrecto',
-              status: 400
-            };
+                  message: 'Numero de maxinmo de intentos alcanzado',
+                  status: HttpStatus.CONFLICT,
+                  nIntentos: this.intento
+                }
           }
-        }
+          else
+          {
+            const data = this.loginService.validLogin(createLoginDto);
+            if((await data) === true)
+            {
+              this.loginService.resetearIntentos(datos.id)
+              this.loginService.crearLogs({accion:'Inicio de sesion',fecha:createLoginDto.fecha,ip:createLoginDto.ip,status:200,url_solicitada:'/login'},datos.email)
+              return {
+                    message: 'Login correcto',
+                    status: 200,
+                    token: datos.id
+                  } 
+            }
+            else
+            return {
+                  message: 'Login incorrecto',
+                  status: 400
+                }
+          }
       }
     } catch (error) {
-      throw new HttpException('Error en el proceso de login', HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new HttpException("El correo no existe", HttpStatus.FOUND);
     }
   }
+
 }
--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { CreateLoginDto } from './dto/create-login.dto';
+import { UpdateLoginDto } from './dto/update-login.dto';
 import { ValidarLogin } from 'src/auth/dto/ValidLoginDto-auth';
 import * as bcryptjs from 'bcryptjs';
 import { AuthService } from 'src/auth/auth.service';
@@ -10,31 +12,45 @@ import { Logs } from 'src/auth/entities/logs.entity';
 @Injectable()
 export class LoginService {
 
-  constructor(
-    @InjectRepository(Auth) private authRepository: Repository<Auth>, 
-    private authService: AuthService,
-    @InjectRepository(Logs) private logsRepository: Repository<Logs>
-  ) { }
+  constructor(@InjectRepository(Auth) private authRepository: Repository<Auth>, private authService: AuthService,
+    @InjectRepository(Logs) private logsRepository: Repository<Logs>) { }
 
   async validLogin(createLoginDto: ValidarLogin): Promise<boolean> {
-    const data = await this.authService.getUser(createLoginDto.email);
-    if (!data) return false; // Verifica si el usuario existe
-    return await bcryptjs.compare(createLoginDto.password, data.password);
+
+    const data = await this.authService.getUser(createLoginDto.email)
+    console.log(createLoginDto)
+    if (await bcryptjs.compare(createLoginDto.password, data.password))
+      return true;
+    else
+      return false;
   }
 
-  async asignarIntentos(id: number, intento: number, fechaUltimoIntento: Date) {
-    await this.authRepository.update(id, { intentos: intento, fechaUltimoIntento: fechaUltimoIntento });
+  asignarIntentos(id: number, intento: number) {
+    this.authRepository.query(
+      "UPDATE usuarios SET intentos = " + intento + " WHERE id = " + id + ""
+    )
   }
 
-  async resetearIntentos(id: number) {
-    await this.authRepository.update(id, { intentos: 0, fechaUltimoIntento: null });
+  resetearIntentos(id: number) {
+    console.log("conteo iniciado")
+    setTimeout(() => {
+      this.authRepository.query(
+        "UPDATE usuarios SET intentos = 0 WHERE id = " + id + ""
+      )
+      console.log("Intentos reseteados")
+    }, 300000)
   }
 
-  async crearLogs(data: {accion: string, ip: string, url_solicitada: string, status: number, fecha: string}, email: string) {
-    const userFound = await this.authRepository.findOne({ where: { email: email } });
-    if (userFound) {
-      const newLog = this.logsRepository.create({ usuario: userFound, ...data });
-      await this.logsRepository.save(newLog);
-    }
+ async crearLogs(data:{accion:string,ip:string,url_solicitada:string,status:number,fecha:string},email:string){
+    const userFound =  await this.authRepository.findOne({
+      where: {
+        email: email
+      }
+    })
+    const newLog = this.logsRepository.create({
+      usuario:userFound,
+      ...data
+    })
+    this.logsRepository.save(newLog)
   }
 }
