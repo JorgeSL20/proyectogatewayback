b4f56f90613fea2e850d1027c3a8bc230ea81561
JorgeSL20 <20201175@uthh.edu.mx>
2024-07-28 03:52:01 -0600

    carajo

---
+++ src/ubicacion/dto/create-ubicacion.dto.ts
@@ -0,0 +1,9 @@
+// src/ubicacion/dto/create-ubicacion.dto.ts
+export class CreateUbicacionDto {
+    id: number;
+    name: string;
+    latitude: number;
+    longitude: number;
+    zoom: number;
+  }
+  
---
+++ src/ubicacion/ubicacion.module.ts
@@ -0,0 +1,14 @@
+// src/ubicacion/ubicacion.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UbicacionService } from './ubicacion.service';
+import { UbicacionController } from './ubicacion.controller';
+import { Ubicacion } from './entities/ubicacion.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Ubicacion])],
+  controllers: [UbicacionController],
+  providers: [UbicacionService],
+  exports: [UbicacionService],
+})
+export class UbicacionModule {}
