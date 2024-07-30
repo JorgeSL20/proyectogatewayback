20c380350430910a0d6f86ffcc23777f03080c1f
KitoS2001 <20200722@uthh.edu.mx>
2024-07-30 03:45:12 -0600

    ultimo

--- src/carrito/carrito.module.ts
+++ src/carrito/carrito.module.ts
@@ -1,17 +1,22 @@
-import { Module } from '@nestjs/common';
-import { CarritoService } from './carrito.service';
-import { CarritoController } from './carrito.controller';
+// src/carrito/carrito.module.ts
+
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Carrito } from './entities/carrito.entity';
-import { Auth } from 'src/auth/entities/auth.entity';
 import { Producto } from 'src/producto/entities/producto.entity';
-import { PagoService } from 'src/pago/pago.service';
-import { Pago } from 'src/pago/entities/pago.entity';
+import { CarritoService } from './carrito.service';
+import { CarritoController } from './carrito.controller';
+import { AuthModule } from 'src/auth/auth.module';  // Importa el AuthModule
+import { PagoModule } from 'src/pago/pago.module';  // Importa el PagoModule
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Carrito, Auth, Producto, Pago])],
+  imports: [
+    TypeOrmModule.forFeature([Carrito, Producto]),  // Incluye las entidades necesarias
+    AuthModule,  // Importa el AuthModule para que el AuthRepository esté disponible
+    forwardRef(() => PagoModule), // Usa forwardRef para PagoModule
+  ],
+  providers: [CarritoService],
   controllers: [CarritoController],
-  providers: [CarritoService, PagoService],
-  exports: [CarritoService],
+  exports: [CarritoService], // Asegúrate de exportar CarritoService si otros módulos lo necesitan
 })
 export class CarritoModule {}
--- src/pago/pago.module.ts
+++ src/pago/pago.module.ts
@@ -1,15 +1,20 @@
+// src/pago/pago.module.ts
 import { Module, forwardRef } from '@nestjs/common';
 import { PagoService } from './pago.service';
 import { PagoController } from './pago.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Pago } from './entities/pago.entity';
+import { AuthModule } from 'src/auth/auth.module'; // Importa AuthModule
 import { CarritoModule } from 'src/carrito/carrito.module';
-import { Auth } from 'src/auth/entities/auth.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Pago, Auth]), forwardRef(() => CarritoModule)],
-  controllers: [PagoController],
+  imports: [
+    TypeOrmModule.forFeature([Pago]),
+    forwardRef(() => CarritoModule),
+    forwardRef(() => AuthModule), // Añade AuthModule aquí
+  ],
   providers: [PagoService],
+  controllers: [PagoController],
   exports: [PagoService],
 })
 export class PagoModule {}
