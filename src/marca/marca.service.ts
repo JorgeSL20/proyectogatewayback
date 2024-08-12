d758349654f7db6371152671b13b7a89b5f0a5c8
KitoS2001 <20200722@uthh.edu.mx>
2024-08-12 11:00:54 -0600

    ultimo

--- src/marca/marca.service.ts
+++ src/marca/marca.service.ts
@@ -12,15 +12,19 @@ export class MarcaService {
   ) {}
 
   async create(createMarcaDto: CreateMarcaDto) {
+    const marcaExistente = await this.marcaRepository.findOne({ where: { marca: createMarcaDto.marca } });
+    if (marcaExistente) {
+      throw new Error('La marca ya existe.');
+    }
     const newMarca = this.marcaRepository.create(createMarcaDto);
     return this.marcaRepository.save(newMarca);
   }
 
-  async remove(id: number) {
-    return this.marcaRepository.delete(id);
-  }
-
-  async updateById(id: number, updateMarcaDto: any) {  // Ajusta el tipo de datos según tu DTO
+  async updateById(id: number, updateMarcaDto: any) {
+    const marcaExistente = await this.marcaRepository.findOne({ where: { marca: updateMarcaDto.marca } });
+    if (marcaExistente && marcaExistente.id !== id) {
+      throw new Error('La marca ya existe.');
+    }
     await this.marcaRepository.update(id, { ...updateMarcaDto });
     return {
       message: 'Marca actualizada correctamente',
@@ -34,5 +38,9 @@ export class MarcaService {
 
   async findOne(id: number) {
     return this.marcaRepository.findOne({ where: { id } });
+  }
+
+  async remove(id: number) {
+    return this.marcaRepository.delete(id);
   }
 }
