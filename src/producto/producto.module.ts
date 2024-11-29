268320bccba6fd187b35cf6dd252c92725131ad2
KitoS2001 <20200722@uthh.edu.mx>
2024-11-29 02:35:44 -0600

    Notificacion

--- src/producto/producto.module.ts
+++ src/producto/producto.module.ts
@@ -1,19 +1,19 @@
-// src/producto/producto.module.ts
 import { Module } from '@nestjs/common';
 import { ProductoService } from './producto.service';
 import { ProductoController } from './producto.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Producto } from './entities/producto.entity';
 import { MulterModule } from '@nestjs/platform-express';
-import { NotificationsModule } from 'src/websocket/notifications.module';// Importa el módulo de notificaciones
+import { NotificationsModule } from 'src/websocket/notifications.module'; // Importa el módulo de notificaciones
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Producto]),
     MulterModule.register(),
+    NotificationsModule, // Importa el módulo correctamente aquí
   ],
   controllers: [ProductoController],
-  providers: [ProductoService,NotificationsModule],
+  providers: [ProductoService], // No necesitas incluir NotificationsModule aquí
   exports: [ProductoService],
 })
 export class ProductoModule {}
