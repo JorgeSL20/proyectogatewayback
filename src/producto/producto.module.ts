cd51ef204940736d8777816f26c2a538678b3aa0
KitoS2001 <20200722@uthh.edu.mx>
2024-12-12 20:05:33 -0600

    Notificacion

--- src/producto/producto.module.ts
+++ src/producto/producto.module.ts
@@ -4,13 +4,15 @@ import { ProductoController } from './producto.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Producto } from './entities/producto.entity';
 import { MulterModule } from '@nestjs/platform-express';
-import { NotificationModule } from 'src/Notificaciones/notification.module'; // Importa el módulo de notificaciones
+import { NotificationModule } from 'src/Notificaciones/notification.module';
+import { SubcategoriaModule } from 'src/subcategoria/subcategoria.module';  // Importa el módulo de notificaciones
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Producto]),
     MulterModule.register(),
-    NotificationModule, // Asegúrate de importar el módulo aquí
+    NotificationModule,
+    SubcategoriaModule // Asegúrate de importar el módulo aquí
   ],
   controllers: [ProductoController],
   providers: [ProductoService],
